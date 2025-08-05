3a14135bf2b4df1129f93a2d0df145a8a55586d7
Qubit Security Inc. <plura@qubitsec.com>
2025-08-05 16:29:38 +0900

    Update run_plura.sh

--- Linux/install/run_plura.sh
+++ Linux/install/run_plura.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo https_proxy=$https_proxy
+
 echo "Switching to root user..."
 sudo -s <<EOF
 
@@ -13,9 +15,13 @@ echo "Registering agent with license key..."
 # 라이선스 키 입력 (여기를 실제 키로 바꾸세요)
 plura register "YOUR_LICENSE_KEY_HERE"
 
+grep license= /etc/plura/conf/.license
+
 echo "Checking agent installation version..."
 /usr/local/sbin/plurad -version
 
 echo "Installation complete."
+
+ps -ef | grep plurad |grep -v grep
 
 EOF
